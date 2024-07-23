416f4cc9770291016cb25411d610d3da852c1fd6
Chetan Singh Rajawat <=chetan.rajawat25@gmail.com>
2024-07-23 20:31:55 +0530

    cors

--- src/App.jsx
+++ src/App.jsx
@@ -84,25 +84,25 @@ import { pushMsg } from './redux/msgSlice';
 function App() {
   const dispatch = useDispatch();
   const me = useSelector((store) => store.user.loggedInUser);
-  let [socket,setSocket]= useState(null);
+  // let [socket,setSocket]= useState(null);
 
-  useEffect(() => {
-    if (me) {
-    const  socket = io("https://lets-chat-backend-7s3j.onrender.com",{
-        query:{
-          userId:me._id,
-                },
-      }); 
-      setSocket(socket)
+  // useEffect(() => {
+  //   if (me) {
+  //   const  socket = io("http://localhost:3000",{
+  //       query:{
+  //         userId:me._id,
+  //               },
+  //     }); 
+  //     setSocket(socket)
  
-      socket.on("getOnlineUsers",(users)=>{
-        console.log(users)
-      dispatch(addOnlineUser(users));
-    })
-      return () => socket.close();
-    } else {
-      if (socket) socket.close();
-    }
+  //     socket.on("getOnlineUsers",(users)=>{
+  //       console.log(users)
+  //     dispatch(addOnlineUser(users));
+  //   })
+  //     return () => socket.close();
+  //   } else {
+  //     if (socket) socket.close();
+  //   }
 
    
 
@@ -110,21 +110,21 @@ function App() {
 
 
 
-  }, [me]);
+  // }, [me]);
 
-  useEffect(() => {
-    let loggedInUser = localStorage.getItem('loggedInUser');
-    if (loggedInUser && !me) {
-      loggedInUser = JSON.parse(loggedInUser);
-      dispatch(addLoggedInUser(loggedInUser));
-    }
-  }, [dispatch, me]);
+  // useEffect(() => {
+  //   let loggedInUser = localStorage.getItem('loggedInUser');
+  //   if (loggedInUser && !me) {
+  //     loggedInUser = JSON.parse(loggedInUser);
+  //     dispatch(addLoggedInUser(loggedInUser));
+  //   }
+  // }, [dispatch, me]);
 
-  useEffect(()=>{
-    socket?.on("newmsg", (msg)=>{
-      dispatch(pushMsg(newmsg))
-    })
-  })
+  // useEffect(()=>{
+  //   socket?.on("newmsg", (msg)=>{
+  //     dispatch(pushMsg(newmsg))
+  //   })
+  // })
 
   return (
     <>
